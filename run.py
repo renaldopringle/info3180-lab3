9e3d9d2c40c9c877abd3a1880e19df40d37fa2fe
Renaldo Pringle <rapchristoff@gmail.com>
2016-03-28 00:03:57 -0500

    update

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-#! /usr/bin/env python
-from app import app
+import os
+from flask import Flask
+from flask import render_template
+from flask import request
+from sendmail import sendmail
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return 'Hello :)'
+
+
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():
+	if request.method == 'GET':
+		return render_template('contact.html')
+	elif (request.method == 'POST'):
+		name=request.form['name']
+        email=request.form['email']
+        subject=request.form['subject']
+        message=request.form['message']
+        sendmail(name,email,subject,message)
+        return "Message sent!"
+
+
 app.run(debug=True,host="0.0.0.0",port=8080)
---
+++ sendmail.py
@@ -0,0 +1,42 @@
+import smtplib
+
+fromname = "Renaldo"
+fromaddr = 'rapchristoff@gmail.com'
+toname = "Renaldo"
+toaddr  = 'renaldopringle@yahoo.com'
+subject = "My Subject"
+message = """From: {} <{}>
+To: {} <{}>
+Subject: {}
+{}""" 
+messagetosend = message.format(
+
+                             fromname,
+
+                             fromaddr,
+
+                             toname,
+
+                             toaddr,
+
+                             subject,
+
+                             message)
+
+# Credentials (if needed)
+
+username = 'myemail@gmail.com'
+
+password = 'mypassword'
+
+# The actual mail send
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+
+server.starttls()
+
+server.login(username,password)
+
+server.sendmail(fromaddr, toaddrs, messagetosend)
+
+server.quit()
